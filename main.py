ee8be0c50e547924c13928d50cb584e9a7bcc7d0
Jollokim <joakimjensenj@gmail.com>
2023-10-20 17:02:37 +0200

    main

---
+++ main.py
@@ -0,0 +1,15 @@
+from utils import BinaryKnapsackProblem
+
+
+def main():
+    problem_name = 'p01' 
+    problem_dir = 'data\p01'
+
+    problem = BinaryKnapsackProblem(problem_name, problem_dir)
+
+    
+
+
+
+if __name__ == '__main__':
+    main()
