ae10e9e58871ff99b577674b845317062a1c7342
Jollokim <joakimjensenj@gmail.com>
2023-10-23 17:03:48 +0200

    testing with GA

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-from utils import BinaryKnapsackProblem
+from utils import BinaryKnapsackProblem, LoggerCSV
+from algorithms import run_ga
 
+
+class DummyConfig:
+    def __init__(self) -> None:
+        self.population_size = 10
+        self.mutation_rate = 0.5
+
+        self.n_cycles = 100
 
 def main():
     problem_name = 'p01' 
     problem_dir = 'data\p01'
 
     problem = BinaryKnapsackProblem(problem_name, problem_dir)
-
+    print('Weights:')
+    print(problem.weights)
+    print('Profits:')
+    print(problem.profits)
+    print('Solution:')
+    print(problem.solution)
+    print('Optimal profit:')
+    print(problem.optimal_profit)
     
+    logger = LoggerCSV(problem)
+
+    config = DummyConfig()
+    
+    print(config.population_size)
+
+    run_ga(problem, config)
+
+
+
+
 
 
 
