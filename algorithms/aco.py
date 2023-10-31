2f4b5f8d218cae4b7df67063d48b69363b933272
Joakim123 Jensen123 <joakimjensenj@gmail.com>
2023-10-31 13:36:45 +0100

    pheromone updating

--- algorithms/aco.py
+++ algorithms/aco.py
@@ -10,10 +10,13 @@ def run_aco(problem: BinaryKnapsackProblem, config, logger: LoggerCSV):
 
     for cycle in range(config.aco.generations):
         ants_walk(ants_population, item_lst, problem.capacity)
-        update_pheromones(ants_population, config.aco.rho)
+        
+        print([item.pheromones for item in item_lst])
+        update_pheromones(ants_population, item_lst, problem.profits, config.aco.rho)
+        print([item.pheromones for item in item_lst])
 
         print([ant.solution for ant in ants_population])
-        quit()
+
 
         
 
@@ -59,7 +62,7 @@ class Ant:
         self.items_picked.append(item)
 
 
-def init_items(problem: BinaryKnapsackProblem):
+def init_items(problem: BinaryKnapsackProblem) -> list[Item]:
 
     item_lst = [] # List()
 
@@ -127,7 +130,7 @@ def create_item_pool(ant: Ant, ant_capacity: int, item_lst: list[Item]):
 
 def selection_probabilities(pool_idx: list[int], item_lst: list[Item], last_item: Item=None):
     probabilities = []
-    sum_attract_phero = sum_for_selection_probabilities(pool_idx, item_lst)
+    sum_attract_phero = sum_for_selection_probabilities(pool_idx, item_lst, last_item)
 
     for idx in pool_idx:
         if last_item is None:
@@ -153,5 +156,42 @@ def sum_for_selection_probabilities(pool_idx: list[int], item_lst: list[Item], l
     return s
 
 
-def update_pheromones(ants_population: list[Ant], evaporation_rate: int):
-    pass
+def update_pheromones(ants_population: list[Ant], item_lst: list[Item], profits: list[int], evaporation_rate: int):
+    ants_value = []
+
+    for ant in ants_population:
+        value = np.sum(ant.solution * profits)
+        ants_value.append(value)
+
+    best_value = np.max(ants_value)
+    best_value_idx = np.argmax(ants_value)
+
+    for i in range(len(ants_population)):
+        update_from_solution(ants_population[i], item_lst, ants_value[i], best_value)
+
+    evaporate_pheromones(item_lst, evaporation_rate)
+
+    
+
+
+
+def update_from_solution(ant: Ant, item_lst: list[Item], ant_value: int, best_value: int):
+    prev_idx = None
+    
+    for i in range(len(ant.items_picked)):
+        if prev_idx is None:
+            item_idx = item_lst.index(ant.items_picked[i])
+            ant.items_picked[i].pheromones[item_idx] += 1 / (1 + (best_value - ant_value / best_value))
+            prev_idx = item_idx
+        else:
+            item_idx = item_lst.index(ant.items_picked[i])
+            ant.items_picked[i].pheromones[prev_idx] += 1 / (1 + (best_value - ant_value / best_value))
+            ant.items_picked[i-1].pheromones[item_idx] += 1 / (1 + (best_value - ant_value / best_value))
+            prev_idx = item_idx
+        
+
+
+def evaporate_pheromones(item_lst: list[Item], evaporation_rate: int):
+    for item in item_lst:
+        for i in range(len(item.pheromones)):
+            item.pheromones[i] *= evaporation_rate
